5848e28dca93eb8d8640db1241d30f78a78b18da
LYSINE <Lysine000@users.noreply.github.com>
2026-07-13 23:54:32 +0530

    kernelsu: add fallthrough, strscpy_pad, TWA_RESUME and task definitions for 4.14 in allowlist

--- KernelSU/kernel/policy/allowlist.c
+++ KernelSU/kernel/policy/allowlist.c
@@ -16,6 +16,27 @@
 #include <linux/compiler_types.h>
 #include <linux/hashtable.h>
 #include <linux/kref.h>
+#include <linux/sched/task.h>
+
+#ifndef TWA_RESUME
+#define TWA_RESUME true
+#endif
+
+#ifndef fallthrough
+#define fallthrough do {} while (0)
+#endif
+
+#ifndef strscpy_pad
+static inline ssize_t ksu_strscpy_pad(char *dest, const char *src, size_t count)
+{
+	ssize_t res = strscpy(dest, src, count);
+	if (res >= 0 && res < count)
+		memset(dest + res, 0, count - res);
+	return res;
+}
+#define strscpy_pad ksu_strscpy_pad
+#endif
+
 
 #include "klog.h" // IWYU pragma: keep
 #include "ksu.h"
