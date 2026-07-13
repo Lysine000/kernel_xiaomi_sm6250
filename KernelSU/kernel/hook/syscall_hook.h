c37aa8a3b8ae5a52c60135619d698d6edc356280
LYSINE <Lysine000@users.noreply.github.com>
2026-07-13 16:53:22 +0530

    fix: syscall event bridge compiler issues (uaccess headers, const syscall_fn_t)

--- KernelSU/kernel/hook/syscall_hook.h
+++ KernelSU/kernel/hook/syscall_hook.h
@@ -5,7 +5,7 @@
 #if defined(__x86_64__)
 typedef sys_call_ptr_t syscall_fn_t;
 #else
-typedef long (*syscall_fn_t)(struct pt_regs *);
+typedef long (*syscall_fn_t)(const struct pt_regs *);
 #endif
 
 extern syscall_fn_t *ksu_syscall_table;
