a465c3769978d049fce324e13a50ecf309ed4d21
LYSINE <Lysine000@users.noreply.github.com>
2026-07-14 04:31:18 +0530

    kernelsu: add copy_from_user_nofault and copy_to_user_nofault for older kernels in util.h

--- KernelSU/kernel/include/util.h
+++ KernelSU/kernel/include/util.h
@@ -21,6 +21,35 @@ static inline long ksys_unshare(unsigned long flags)
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(5, 8, 0)
 #define strncpy_from_user_nofault(dst, src, count) strncpy_from_unsafe((dst), (const void *)(src), (count))
+
+static inline long copy_from_user_nofault(void *dst, const void __user *src, size_t size)
+{
+	long ret;
+	mm_segment_t old_fs = get_fs();
+
+	set_fs(USER_DS);
+	pagefault_disable();
+	ret = __copy_from_user_inatomic(dst, src, size);
+	pagefault_enable();
+	set_fs(old_fs);
+
+	return ret ? -EFAULT : 0;
+}
+
+static inline long copy_to_user_nofault(void __user *dst, const void *src, size_t size)
+{
+	long ret;
+	mm_segment_t old_fs = get_fs();
+
+	set_fs(USER_DS);
+	pagefault_disable();
+	ret = __copy_to_user_inatomic(dst, src, size);
+	pagefault_enable();
+	set_fs(old_fs);
+
+	return ret ? -EFAULT : 0;
+}
 #endif
+
 
 #endif
